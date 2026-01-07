72d4399e7fff0a337aa09b98fa91f95e4c735a99
bryan0060 <ojalaifood@gmail.com>
2026-01-06 21:34:00 -0500

    Fix STATIC_ROOT error for Render

--- rh_django/settings.py
+++ rh_django/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:    # Solo en producción
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --- CORS ---
